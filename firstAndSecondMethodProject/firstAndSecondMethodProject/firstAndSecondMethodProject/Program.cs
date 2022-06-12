a7fe5c0ff837349977b4f9979c4db5b28b031d6c
Ceyhun <contact.ceyhun@gmail.com>
2022-06-12 18:01:04 +0400

    added method projects

---
+++ firstAndSecondMethodProject/firstAndSecondMethodProject/firstAndSecondMethodProject/Program.cs
@@ -0,0 +1,32 @@
+﻿char[] alphabetUpper = { 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z' };
+
+char[] alphabetLower = { 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z' };
+while (true)
+{
+    Console.Write("PLS INSERT FIRST TEXT FOR CHANGE TO UPPER CHARS : ");
+    string targetText = Console.ReadLine();
+    string result = AlphabetChanger(targetText, alphabetUpper, alphabetLower);
+    Console.WriteLine($"Your changed name : {result}");
+
+    Console.Write("PLS INSERT SECOND TEXT FOR CHANGE TO LOWER CHARS : ");
+    string targetText2 = Console.ReadLine();
+    string result2 = AlphabetChanger2(targetText2, alphabetLower, alphabetUpper);
+    Console.WriteLine($"Your changed name : {result2}");
+}
+static string AlphabetChanger(string suitableText, char[] upperAlphabet, char[] lowerAlphabet)
+{
+    for (int i = 0; i < upperAlphabet.Length; i++)
+    {
+        suitableText = suitableText.Replace(lowerAlphabet[i], upperAlphabet[i]);
+    }
+    return suitableText;
+}
+
+static string AlphabetChanger2(string suitableText, char[] lowerAlphabet, char[] upperAlphabet)
+{
+    for (int i = 0; i < lowerAlphabet.Length; i++)
+    {
+        suitableText = suitableText.Replace(upperAlphabet[i], lowerAlphabet[i]);
+    }
+    return suitableText;
+}
---
+++ thirdMethodProject/thirdMethodProject/thirdMethodProject/Program.cs
@@ -0,0 +1,49 @@
+﻿while(true) { 
+Console.WriteLine("Whould u like to cont with default mode ? press yes or no");
+string targetForm = Console.ReadLine();
+bool isCaseSensetive = false;
+
+if (targetForm == "yes")
+{
+    isCaseSensetive = true;
+}
+else if (targetForm == "no")
+{
+    isCaseSensetive = false;
+}
+
+Console.Write("INSERT TARGET TEXT : ");
+string targetText = Console.ReadLine();
+
+Console.Write("Insert target char : ");
+string targetChar = Console.ReadLine();
+
+bool result = CharChecker(targetText, targetChar, isCaseSensetive);
+Console.WriteLine(result);
+}
+
+static bool CharChecker(string targetText, string targetChar, bool isCaseSensetive)
+{
+    for (int i = 0; i < targetText.Length; i++)
+    {
+        if (isCaseSensetive == false)
+        {
+            targetText = targetText.ToLower();
+            targetChar = targetChar.ToLower();
+            if ($"{targetText[i]}" == targetChar)
+            {
+                return true;
+            }
+
+        }
+        else if (isCaseSensetive == true)
+        {
+            if ($"{targetText[i]}" == targetChar)
+            {
+                return true;
+            }
+
+        }
+    }
+    return false;
+}
